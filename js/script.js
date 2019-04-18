1f1ffc5aef0310392fa6854c1fb814efad3b74f7
Stephen P <stephenparkhum@outlook.com>
2019-04-17 19:08:53 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,44 +33,6 @@ let newsClick = function () {
 
 const mainDiv = document.getElementsByClassName('main');
 
-// let artistsPage = function () {
-
-//     let div = document.createElement('div');
-//     // let ul = document.createElement('ul');
-//     // let li = document.createElement('li');
-//     let h4 = document.createElement('h4');
-//     let table = document.createElement('table');
-//     let tr = document.createElement('tr');
-//     let td = document.createElement('td');
-//     document.getElementsByClassName('main_in_wrapper')[0].appendChild(div);
-//     div.classList.add('artist_list');
-//     div.appendChild(table);
-//     // ul.appendChild(li);
-//     table.appendChild(tr);
-
-
-//     for (let i = 0; i < bands.length; i++) {
-//         // let artistNames = document.createElement('h4');
-//         // div.appendChild(artistNames);
-//         let artist_names = bands[i].artist;
-//         tr.appendChild(td);
-//         td.appendChild(h4);
-//         h4.innerHTML = '<h4>' + bands[i].artist + '</h4>';
-//         console.log(artist_names + ' - ' + bands[i].genre);
-//     }
-
-    
-
-
-
-
-
-//     // let h1 = document.createElement('h1');
-//     // for (let i = 0; i < artists.length; i++) {
-//     //     console.log(artist[0].name);
-//     // }
-// };
-
 artistsClick();
 newsClick();
 // console.log(bands);
