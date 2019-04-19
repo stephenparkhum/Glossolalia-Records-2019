18c533b87bb6d21b6818290a91b42ee6ac6545fe
Stephen P <stephenparkhum@outlook.com>
2019-04-18 19:46:39 -0700

    Updating artist modal
    Working on modal pop-up information

--- js/script.js
+++ js/script.js
@@ -46,7 +46,7 @@ let eventsClick = function () {
             news[i].style.display = 'none';
         }
         let eventsPage = document.getElementsByClassName("events-page");
-        eventsPage.style.display = 'block';
+        eventsPage[0].style.display = 'block';
     });
 };
 
@@ -55,7 +55,11 @@ const mainDiv = document.getElementsByClassName('main');
 artistsClick();
 newsClick();
 eventsClick();
-// console.log(bands);
+
+
+
+
+
 
 // BUTTON / POP-UP
 
@@ -71,16 +75,23 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function () {
     modal.style.display = "block";
-}
+    let modalHeader = modal.getElementsByClassName('modal-header');
+    let header = modal.getElementsByClassName('artist-name');
+    header.textContent = 'Fuck you';
+    // modalHeader.appendChild(h2);
+    // let p = document.createElement('p');
+    // var artistName = document.getElementsByClassName('artist-name');
+    // modal.appendChild(pInner);
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
