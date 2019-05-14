c88e9be969bff491c8c756e799354e0e1cb08d0b
Stephen P <stephenparkhum@outlook.com>
2019-05-14 10:49:35 -0700

    Navigation update
    <strong>Nav Update</strong>
    Adding more functionality and additional links to the navigation section of the landing page.

--- js/script.js
+++ js/script.js
@@ -2,23 +2,6 @@
 const mainDiv = document.getElementsByClassName('main');
 
 
-
-// SHOW / HIDE FUNCTIONS
-
-
-// -- ARTISTS SHOW/HIDE--
-// let artistShow = () => {
-//     let artistsPages = document.getElementsByClassName('artists_page');
-//     artistsPages[0].style.display = 'block';
-// };
-
-// let artistHide = () => {
-//     let elem = document.getElementsByClassName('artists_page');
-//     elem[0].style.display = 'none';
-// };
-
-
-
 // ARTISTS PAGE
 let artistsClick = function () {
     let artists_link = document.getElementById('artists');
@@ -60,53 +43,19 @@ let newsClick = function () {
 let contactsClick = function () {
     let contacts_nav = document.getElementById('contact-nav');
     contacts_nav.addEventListener('click', function () {
-        let contact = document.getElementsByClassName('contacts-page');
-        contact[0].style.display = "block";
-        let elem = document.getElementsByClassName('recent');
+        let elem = document.getElementsByClassName("recent");
         elem[0].style.display = "none";
-        let artists = document.getElementsByClassName('artists_page');
+        let artists = document.getElementsByClassName("artists_page");
         artists[0].style.display = "none";
-        let news = document.getElementsByClassName('news');
+        let news = document.getElementsByClassName("news");
         for (i = 0; i < news.length; i++) {
             news[i].style.display = "none";
         }
+        let contact = document.getElementsByClassName("contacts-page");
+        contact[0].style.display = "block";
     });
 };
 
 artistsClick();
 newsClick();
 contactsClick();
-
-
-// BUTTON / POP-UP
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
-    let mailingList = document.getElementsByClassName('mailing-list');
-    mailingList[0].style.display = "none";
-
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-    let mailingList = document.getElementsByClassName('mailing-list');
-    mailingList[0].style.display = "block";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
